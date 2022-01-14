3a7694f1a05ae685b39c1176be1890b82794840e
FurqanAthar <22100070@lums.edu.pk>
2022-01-15 01:09:34 +0500

    Edit/Delete/Filter Animal, Update Profile Password

--- Prototype/Development/frontend/src/actions/userAction.js
+++ Prototype/Development/frontend/src/actions/userAction.js
@@ -7,6 +7,12 @@ import {
   USER_UPDATENAME_REQUEST,
   USER_UPDATENAME_SUCCESS,
   USER_UPDATENAME_FAIL,
+  USER_UPDATEPASSWORD_REQUEST,
+  USER_UPDATEPASSWORD_SUCCESS,
+  USER_UPDATEPASSWORD_FAIL,
+  USER_UPDATEIMAGE_REQUEST,
+  USER_UPDATEIMAGE_SUCCESS,
+  USER_UPDATEIMAGE_FAIL,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -24,15 +30,15 @@ export const login = (email, password) => async (dispatch) => {
     dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
 
     localStorage.setItem("loginInfo", JSON.stringify(data));
-    } catch (error) {
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload:
-                error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message,
-        });
-    }
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export const logout = () => (dispatch) => {
@@ -50,13 +56,15 @@ export const userUpdateName = (name) => async (dispatch, getState) => {
       login: { loginInfo },
     } = getState();
 
-    const config = { headers: { 
-      "Content-Type": "application/json",
-      Authorization: loginInfo.token
-    }};
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: loginInfo.token,
+      },
+    };
 
     const { data } = await axios.put(
-      "/farm/update/user/name",
+      "/farm/user/update/name",
       { name },
       config
     );
@@ -68,6 +76,88 @@ export const userUpdateName = (name) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: USER_UPDATENAME_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const userUpdatePassword =
+  ({ oldPassword, password }) =>
+  async (dispatch, getState) => {
+    try {
+      dispatch({ type: USER_UPDATEPASSWORD_REQUEST });
+
+      const {
+        login: { loginInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: loginInfo.token,
+        },
+      };
+
+      const { data } = await axios.put(
+        "/farm/user/update/password",
+        { oldPassword, password },
+        config
+      );
+
+      if (data.success) {
+        dispatch({ type: USER_UPDATEPASSWORD_SUCCESS, payload: data });
+        dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+      } else {
+        dispatch({ type: USER_UPDATEPASSWORD_FAIL, payload: data.message });
+      }
+
+      localStorage.setItem("loginInfo", JSON.stringify(data));
+    } catch (error) {
+      dispatch({
+        type: USER_UPDATEPASSWORD_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+export const userUpdateImage = (image) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATEIMAGE_REQUEST });
+
+    const {
+      login: { loginInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: loginInfo.token,
+      },
+    };
+
+    const { data } = await axios.put(
+      "/farm/user/update/image",
+      { image },
+      config
+    );
+
+    if (data.success) {
+      dispatch({ type: USER_UPDATEIMAGE_SUCCESS, payload: data });
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    } else {
+      dispatch({ type: USER_UPDATEIMAGE_FAIL, payload: data.message });
+    }
+
+    localStorage.setItem("loginInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATEIMAGE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- Prototype/Development/frontend/src/reducers/userReducer.js
+++ Prototype/Development/frontend/src/reducers/userReducer.js
@@ -1,13 +1,19 @@
-import axios from 'axios'
-import { 
-    USER_LOGIN_REQUEST, 
-    USER_LOGIN_SUCCESS, 
-    USER_LOGIN_FAIL, 
-    USER_LOGOUT,
-    USER_UPDATENAME_SUCCESS,
-    USER_UPDATENAME_REQUEST,
-    USER_UPDATENAME_FAIL
-} from '../constants/userConstants'
+import axios from "axios";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  USER_UPDATENAME_SUCCESS,
+  USER_UPDATENAME_REQUEST,
+  USER_UPDATENAME_FAIL,
+  USER_UPDATEPASSWORD_SUCCESS,
+  USER_UPDATEPASSWORD_FAIL,
+  USER_UPDATEPASSWORD_REQUEST,
+  USER_UPDATEIMAGE_REQUEST,
+  USER_UPDATEIMAGE_SUCCESS,
+  USER_UPDATEIMAGE_FAIL,
+} from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
@@ -35,4 +41,30 @@ export const userUpdateProfileReducer = (state = {}, action) => {
     default:
       return state;
   }
-}
+};
+
+export const userUpdatePasswordReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATEPASSWORD_REQUEST:
+      return { loading: true };
+    case USER_UPDATEPASSWORD_SUCCESS:
+      return { loading: false, loginInfo: action.payload, success: true };
+    case USER_UPDATEPASSWORD_FAIL:
+      return { loading: false, message: action.payload, success: false };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateImageReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATEIMAGE_REQUEST:
+      return { loading: true };
+    case USER_UPDATEIMAGE_SUCCESS:
+      return { loading: false, loginInfo: action.payload, success: true };
+    case USER_UPDATEIMAGE_FAIL:
+      return { loading: false, message: action.payload, success: false };
+    default:
+      return state;
+  }
+};
