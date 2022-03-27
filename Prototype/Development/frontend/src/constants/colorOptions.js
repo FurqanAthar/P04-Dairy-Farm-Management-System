94f26e9cab68bc69d50477ccda7efe2ffc6289ae
Furqan Athar <furqanqazi.33@gmail.com>
2022-03-27 19:52:28 +0500

    Milk Supply Done, Graphs with filtration Done

---
+++ Prototype/Development/frontend/src/constants/colorOptions.js
@@ -0,0 +1,23 @@
+export const redCombination = {
+  dark: "#ff4859",
+  light: "#ffe1e4",
+  lightRgb: "rgb(255, 72, 89, 0.2)",
+};
+
+export const blueCombination = {
+  dark: "#519dfc",
+  light: "#e7f2ff",
+  lightRgb: "rgb(81, 157, 252, 0.3)",
+};
+
+export const greenCombination = {
+  dark: "#2cd48e",
+  light: "#d5f7e9",
+  lightRgb: "rgb(44, 212, 142, 0.2)",
+};
+
+export const purpleCombination = {
+  dark: "#7848FF",
+  light: "#e9e1ff",
+  lightRgb: "rgb(120, 72, 255, 0.2)",
+};
